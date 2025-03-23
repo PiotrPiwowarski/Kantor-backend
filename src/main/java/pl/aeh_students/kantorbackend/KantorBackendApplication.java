37ccff335e6d79c0df38ce9a0695e25fb1868424
Piotr Piwowarski <piotr@piwowarski-uniet.com>
2025-03-23 08:43:22 +0100

    initial commit

--- src/main/java/pl/aeh_students/kantorbackend/KantorBackendApplication.java
+++ src/main/java/pl/aeh_students/kantorbackend/KantorBackendApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class KantorBackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(KantorBackendApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(KantorBackendApplication.class, args);
+    }
 
 }
--- src/test/java/pl/aeh_students/kantorbackend/KantorBackendApplicationTests.java
+++ src/test/java/pl/aeh_students/kantorbackend/KantorBackendApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class KantorBackendApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
